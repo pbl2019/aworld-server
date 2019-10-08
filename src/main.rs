ce2dfe9b8222c4bc6cfbf099cefae86e40ed6e07
tomatoaiu <boushi_sukaimu@yahoo.co.jp>
2019-10-08 17:53:27 +0900

    feat(udp): send data to control server

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,51 @@
 use std::net::UdpSocket;
 use std::net::SocketAddr;
+use std::thread;
 
-struct Server {
-    ipaddr: SocketAddr,
-    buf: [u8; 10]
-}
-
-impl Server {
-    fn bind(&mut self) -> UdpSocket {
-        return UdpSocket::bind(self.ipaddr).expect("couldn't bind to address");
-    }
-
-    fn reseive(&mut self, socket: UdpSocket) -> Client {
-        let (number_of_bytes, src_addr) = socket.recv_from(&mut self.buf).expect("Didn't receive data");
-
-        return Client {
-            ipaddr: src_addr,
-            size: number_of_bytes,
-            buf: self.buf
-        }
-    }
-}
-
-struct Client {
-    ipaddr: SocketAddr,
-    size: usize,
-    buf: [u8; 10]
-}
+mod opt;
 
 fn main() {
-    let mut server = Server {
+    mock_data_server();
+
+    let mut server = opt::Server {
         ipaddr: SocketAddr::from(([127, 0, 0, 1], 34254)),
         buf: [0; 10]
     };
     let socket = server.bind();
+
     loop {
         let client = server.reseive(socket.try_clone().expect("failed to clone socket"));
-        println!("{:?} from message, usize: {:?}, buf: {:?}", client.ipaddr, client.size, client.buf);
+        println!("{:?} from Client, usize: {:?}, buf: {:?}", client.ipaddr, client.size, client.buf);
+
+        thread::spawn(move || {
+            let mut send_server = opt::Server {
+                ipaddr: SocketAddr::from(([127, 0, 0, 1], 30000)),
+                buf: [0; 10]
+            };
+            let send_socket = send_server.bind();
+            send_socket.send_to(&[0; 10], SocketAddr::from(([127, 0, 0, 1], 20202))).expect("couldn't send data");
+        });
     }
 }
 
 #[test]
-fn it_works() {
+fn mock_client() {
     let socket = UdpSocket::bind("127.0.0.1:12345").expect("couldn't bind to address");
     socket.send_to(&[1, 2, 5], "127.0.0.1:34254").expect("couldn't send data");
+}
+
+// #[test]
+fn mock_data_server() {
+    thread::spawn(|| {
+        let mut server = opt::Server {
+            ipaddr: SocketAddr::from(([127, 0, 0, 1], 20202)),
+            buf: [0; 10]
+        };
+        let socket = server.bind();
+
+        loop {
+            let client = server.reseive(socket.try_clone().expect("failed to clone socket"));
+            println!("{:?} from Control Server, usize: {:?}, buf: {:?}", client.ipaddr, client.size, client.buf);
+        }
+    });
 }
---
+++ src/opt.rs
@@ -0,0 +1,29 @@
+use std::net::UdpSocket;
+use std::net::SocketAddr;
+
+pub struct Server {
+    pub ipaddr: SocketAddr,
+    pub buf: [u8; 10]
+}
+
+impl Server {
+    pub fn bind(&mut self) -> UdpSocket {
+        return UdpSocket::bind(self.ipaddr).expect("couldn't bind to address");
+    }
+
+    pub fn reseive(&mut self, socket: UdpSocket) -> Client {
+        let (number_of_bytes, src_addr) = socket.recv_from(&mut self.buf).expect("Didn't receive data");
+
+        return Client {
+            ipaddr: src_addr,
+            size: number_of_bytes,
+            buf: self.buf
+        }
+    }
+}
+
+pub struct Client {
+    pub ipaddr: SocketAddr,
+    pub size: usize,
+    pub buf: [u8; 10]
+}
