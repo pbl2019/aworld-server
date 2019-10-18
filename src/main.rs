083639d8516f518d2e1563cbc3424a71f7906491
tomatoaiu <boushi_sukaimu@yahoo.co.jp>
2019-10-18 16:40:49 +0900

    feat(dataformat): receive control information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
 use std::net::SocketAddr;
 use std::thread;
 
@@ -6,38 +11,48 @@ mod opt;
 fn main() {
     let mut server = opt::Server {
         ipaddr: SocketAddr::from(([127, 0, 0, 1], 34254)),
-        buf: [0; 10]
+        buf: [0; 1000]
     };
     let socket = server.bind();
 
     loop {
         let client = server.receive(socket.try_clone().expect("failed to clone socket"));
-        println!("{:?} from Client, usize: {:?}, buf: {:?}", client.ipaddr, client.size, client.buf);
+        println!("{:?} from Client, usize: {:?}", client.ipaddr, client.size);
+        let control_info = opt::ControlInfo::deserialize(client.buf);
+        println!("ControlInfo: {:?}", control_info);
 
         thread::spawn(move || {
             let mut send_server = opt::Server {
                 ipaddr: SocketAddr::from(([127, 0, 0, 1], 30000)),
-                buf: [0; 10]
+                buf: [0; 1000]
             };
             let send_socket = send_server.bind();
-            send_socket.send_to(&[0; 10], SocketAddr::from(([127, 0, 0, 1], 20202))).expect("couldn't send data");
+            let byte = opt::ControlInfo::serialize(control_info);
+            send_socket.send_to(&byte, SocketAddr::from(([127, 0, 0, 1], 20202))).expect("couldn't send data");
         });
     }
 }
 
 #[test]
-fn mock_data_server() {
+fn mock_client_and_mock_data_server() {
     let handle = thread::spawn(|| {
         // 仮のデータサーバを立てる
         let mut server = opt::Server {
             ipaddr: SocketAddr::from(([127, 0, 0, 1], 20202)),
-            buf: [0; 10]
+            buf: [0; 1000]
         };
         let socket = server.bind();
 
         // 仮のクライアントからデータを送信
         let client = std::net::UdpSocket::bind("127.0.0.1:12345").expect("couldn't bind to address");
-        client.send_to(&[1, 2, 5], "127.0.0.1:34254").expect("couldn't send data");
+        let control_info = opt::ControlInfo {
+            character_id: "aworld client".to_string(),
+            button_name: "enter".to_string(),
+            status: true,
+            optional: "hoo,bar".to_string()
+        };
+        let byte = opt::ControlInfo::serialize(control_info);
+        client.send_to(&byte, "127.0.0.1:34254").expect("couldn't send data");
         println!("Send from Client");
 
         // コントロールサーバーからの受信
--- src/opt.rs
+++ src/opt.rs
@@ -1,9 +1,10 @@
 use std::net::UdpSocket;
 use std::net::SocketAddr;
+use std::str;
 
 pub struct Server {
     pub ipaddr: SocketAddr,
-    pub buf: [u8; 10]
+    pub buf: [u8; 1000]
 }
 
 impl Server {
@@ -17,7 +18,7 @@ impl Server {
         return Client {
             ipaddr: src_addr,
             size: number_of_bytes,
-            buf: self.buf
+            buf: self.buf.to_vec()
         }
     }
 }
@@ -25,5 +26,30 @@ impl Server {
 pub struct Client {
     pub ipaddr: SocketAddr,
     pub size: usize,
-    pub buf: [u8; 10]
+    pub buf: Vec<u8>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ControlInfo {
+    pub character_id: String,
+    pub button_name: String,
+    pub status: bool,
+    pub optional: String
+}
+
+impl ControlInfo {
+    pub fn deserialize(buf: Vec<u8>) -> ControlInfo {
+        let control_info_str = str::from_utf8(&buf).expect("Found invalid UTF-8");
+        // JSON has non-whitespace trailing characters after the value.
+        // https://stackoverflow.com/questions/56817010/why-do-i-always-get-a-trailing-characters-error-when-trying-to-parse-data-with
+        let control_info_trimed_str = control_info_str.trim_matches(char::from(0));
+        let control_info: ControlInfo = serde_json::from_str(control_info_trimed_str).unwrap();
+        control_info
+    }
+
+    pub fn serialize(control_info: ControlInfo) -> Vec<u8> {
+        let json = serde_json::to_string(&control_info).unwrap();
+        let byte = json.as_bytes();
+        return byte.to_vec();
+    }
 }
