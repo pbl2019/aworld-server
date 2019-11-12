16d81eb079641ad5e8ff6cb03ed2f1b3fbbd4834
tamamu <tamamu.1r1s@gmail.com>
2019-11-12 21:50:41 +0900

    It works

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,128 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
-use std::net::SocketAddr;
+use std::collections::HashMap;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::sync::{Arc, RwLock};
 use std::thread;
 
 mod opt;
 
+#[derive(Debug, Copy, Clone)]
+pub struct Controller {
+    pub login: bool,
+    pub up: bool,
+    pub down: bool,
+    pub right: bool,
+    pub left: bool,
+}
+
+impl Controller {
+    fn new() -> Self {
+        Self {
+            login: false,
+            up: false,
+            down: false,
+            right: false,
+            left: false,
+        }
+    }
+}
+
+impl std::iter::IntoIterator for Controller {
+    type Item = (String, bool);
+    type IntoIter = ::std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        vec![
+            ("login".to_owned(), self.login),
+            ("forward".to_owned(), self.up),
+            ("down".to_owned(), self.down),
+            ("right".to_owned(), self.right),
+            ("left".to_owned(), self.left),
+        ]
+        .into_iter()
+    }
+}
+
 fn main() {
     let mut server = opt::Server {
         ipaddr: SocketAddr::from(([127, 0, 0, 1], 34255)),
-        buf: [0; 4096]
+        buf: [0; 4096],
     };
     let socket = server.bind();
+    let controllers: Arc<RwLock<HashMap<String, Arc<RwLock<Controller>>>>> =
+        Arc::new(RwLock::new(HashMap::new()));
+    let controllers2 = controllers.clone();
+
+    let mut send_server = opt::Server {
+        ipaddr: SocketAddr::from(([127, 0, 0, 1], 34250)),
+        buf: [0; 4096],
+    };
+    let send_socket = send_server.bind();
+
+    thread::spawn(move || loop {
+        let controllers_lock = controllers2.read().unwrap();
+        for (ip, controller) in controllers_lock.iter() {
+            let mut lock = controller.write().unwrap();
+            for (kind, should_send) in lock.into_iter() {
+                if should_send {
+                    if kind == "login" {
+                        lock.login = false;
+                    }
+                    let buf = format!(
+                        r#"{{
+                    "addr": "{}",
+                    "kind": "{}",
+                    "payload": {{ "speed": 1.0 }}
+                }}"#,
+                        ip, kind
+                    );
+                    send_socket
+                        .send_to(buf.as_bytes(), SocketAddr::from(([127, 0, 0, 1], 34254)))
+                        .expect("couldn't send data");
+                }
+            }
+        }
+        thread::sleep_ms(5);
+    });
 
     loop {
         let client = server.receive(socket.try_clone().expect("failed to clone socket"));
         println!("{:?} from Client, usize: {:?}", client.ipaddr, client.size);
+        let ip = match client.ipaddr {
+            SocketAddr::V4(v4) => v4.to_string(),
+            SocketAddr::V6(v6) => v6.to_string(),
+        };
         let control_info = opt::ControlInfo::deserialize(client.buf);
         println!("ControlInfo: {:?}", control_info);
-
-        thread::spawn(move || {
-            let mut send_server = opt::Server {
-                ipaddr: SocketAddr::from(([127, 0, 0, 1], 34250)),
-                buf: [0; 4096]
-            };
-            let send_socket = send_server.bind();
-            let byte = opt::ControlInfo::serialize(control_info);
-            send_socket.send_to(&byte, SocketAddr::from(([127, 0, 0, 1], 34254))).expect("couldn't send data");
-        });
+        let ip_is_not_found = !controllers.read().unwrap().contains_key(&ip);
+        if ip_is_not_found {
+            controllers
+                .write()
+                .unwrap()
+                .insert(ip.clone(), Arc::new(RwLock::new(Controller::new())));
+        }
+        if let Some(controller) = controllers.read().unwrap().get(&ip) {
+            match &*control_info.button_name {
+                "login" => {
+                    controller.write().unwrap().login = control_info.status;
+                }
+                "up" => {
+                    controller.write().unwrap().up = control_info.status;
+                }
+                "down" => {
+                    controller.write().unwrap().down = control_info.status;
+                }
+                "left" => {
+                    controller.write().unwrap().left = control_info.status;
+                }
+                "right" => {
+                    controller.write().unwrap().right = control_info.status;
+                }
+                _ => {}
+            }
+        }
     }
 }
 
@@ -39,29 +134,34 @@ fn mock_client_and_mock_data_server() {
         // 仮のデータサーバを立てる
         let mut server = opt::Server {
             ipaddr: SocketAddr::from(([127, 0, 0, 1], 20202)),
-            buf: [0; 4096]
+            buf: [0; 4096],
         };
         let socket = server.bind();
 
         // 仮のクライアントからデータを送信
-        let client = std::net::UdpSocket::bind("127.0.0.1:12345").expect("couldn't bind to address");
+        let client =
+            std::net::UdpSocket::bind("127.0.0.1:12345").expect("couldn't bind to address");
         let control_info = opt::ControlInfo {
             character_id: "aworld client".to_string(),
             button_name: "enter".to_string(),
             status: true,
-            optional: "hoo,bar".to_string()
+            optional: "hoo,bar".to_string(),
         };
         let byte = opt::ControlInfo::serialize(control_info);
-        client.send_to(&byte, "127.0.0.1:34254").expect("couldn't send data");
+        client
+            .send_to(&byte, "127.0.0.1:34254")
+            .expect("couldn't send data");
         println!("Send from Client");
 
         // コントロールサーバーからの受信
         loop {
             let client = server.receive(socket.try_clone().expect("failed to clone socket"));
-            println!("{:?} from Control Server, usize: {:?}, buf: {:?}", client.ipaddr, client.size, client.buf);
+            println!(
+                "{:?} from Control Server, usize: {:?}, buf: {:?}",
+                client.ipaddr, client.size, client.buf
+            );
         }
     });
 
     assert!(handle.join().is_ok());
 }
-
